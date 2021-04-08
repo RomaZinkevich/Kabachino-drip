83f6992af12e12655564b6f963aa2da58df43eec
RomaZinkevich <zra1903@mail.ru>
2021-04-08 20:44:52 +0300

    v1.0

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+import sys
+import json
+import random
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'adgfadfd'
+
+
+class SearchForm(FlaskForm):
+    search = StringField('', validators=[DataRequired()])
+    submit = SubmitField('Искать')
+
+
+@app.route("/", methods=['POST', 'GET'])
+def main_page():
+    form = SearchForm()
+    if form.validate_on_submit():
+        print(form.search)
+        return 'АХАХАХАХАХА'
+    return render_template('main.html', title='PANOS', form=form)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1', debug=True)
